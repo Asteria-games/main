7b1123c502238daa35a362c54143f03be8c46553
Nervous Nightingale <nightingale@nervous.localdomain>
2021-10-31 18:43:29 +0100

    Main site

---
+++ pages/3Drender/render.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Model() {
+    return(
+        <iframe allowFullScreen width="640" height="480" src="https://p3d.in/e/nx5MK+clean+spin+load" className="min-h-screen flex items-top justify-center"></iframe>
+    )
+}
+
+export default Model
---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Homepage() {
+    return (
+    <>
+    <header>
+        <div className='min-h-screen flex items-top justify-end bg-gray-700'>
+            <div className="w-64 h-64 bg-gray-400 rounded-full"></div>
+        </div>
+        <div className="min-h-screen flex items-top justify-start bg-gray-600"></div>
+    </header>
+    </>
+    );
+}
+
+export default Homepage
